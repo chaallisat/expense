a812dadcf205e20544f09b630492ed7224ff9545
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-07-12 01:14:44 -0700

    added to the html and psudeocoded js

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+
+
+//Store all info gathered from UI
+const category = $("#category").val().trim();
+const exName = $("#ex-name").val().trim();
+const amount = $("#amount").val().trim();
+const exDate = $("#ex-date").val().trim();
+
+console.log(category);
+console.log(exName);
+console.log(amount);
+console.log(exDate);
+
+
+
+$("#bacon").append(exName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$("#add-expense").on("click", function(event) {
+    event.preventDefault();
+})*/
+//Display info in table
+
+//Export info into excel spreadsheet
